1f38c561c79cacb57436ce5241138e90daf43487
Richard Phoon <richardphoon121@gmail.com>
2021-01-27 16:57:57 +0800

    updated fetch url

--- src/App.js
+++ src/App.js
@@ -76,7 +76,7 @@ class App extends React.Component {
 
   onButtonSubmit = () => {
     this.setState({imageUrl: this.state.input});
-    fetch('http://localhost:3000/imageurl', {
+    fetch('https://whispering-temple-56003.herokuapp.com/imageurl', {
             method: 'post',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
@@ -86,7 +86,7 @@ class App extends React.Component {
       .then(response => response.json())
       .then(response => {
         if (response) {
-          fetch('http://localhost:3000/image', {
+          fetch('https://whispering-temple-56003.herokuapp.com/image', {
             method: 'put',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
